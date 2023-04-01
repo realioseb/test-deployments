91b909fb5588654188f634d23835a02e062024f1
realioseb <soso.bichinashvili@gmail.com>
2023-04-01 14:46:19 +0400

    failed test

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -4,6 +4,6 @@ import { sum } from "./utils";
 describe("utils", () => {
   it("should sum up two numbers correctly", () => {
     const number = sum(1, 1);
-    expect(number).toEqual(2);
+    expect(number).toEqual(4);
   });
 });
