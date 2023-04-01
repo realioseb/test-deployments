bf76a40b49260cd2dcfdd005a3ba4170fdf77f13
realioseb <soso.bichinashvili@gmail.com>
2023-04-01 14:03:59 +0400

    feat: setup simple tests

---
+++ src/utils.spec.ts
@@ -0,0 +1,9 @@
+import "jest";
+import { sum } from "./utils";
+
+describe("utils", () => {
+  it("should sum up two numbers correctly", () => {
+    const number = sum(1, 1);
+    expect(number).toEqual(2);
+  });
+});
